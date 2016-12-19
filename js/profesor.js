0f37bb9c64ac2260f909aa43ff8fd116ed500f94
Freddy Samaniego <fsama21@outlook.com>
2016-12-19 09:40:01 -0500

    fsama21: Lista de proyectos profesor

---
+++ js/profesor.js
@@ -0,0 +1,35 @@
+
+
+function agregarProyecto(){
+	
+	if ( $("#descrip").val() === "" || $("#titulo").val() === "" || $("#autor").val() === "" || $("#etiq").val() === "" ) {
+		console.log("campos vacios");
+		return;		
+	}
+
+	$('#lista_Gener ul').append(
+	    $('<li>').attr('class','list-group-item list-group-item-info').append(
+	        $("#titulo").val()
+	    )
+	)	
+	
+	$('#lista_Prof ul').append(
+	    $('<li>').attr('class','list-group-item list-group-item-info').append(	        
+	        $("<a>").attr('href','#').append(
+	        	$('<span>').attr('class', 'label label-warning').append("Edit")
+	        ),$("<a>").attr('href','#').append(
+	        	$('<span>').attr('class', 'label label-danger').append("Delete")
+	        ), $("#titulo").val()
+	    )
+	)
+
+	console.log("Se agrego elemento");
+}
+
+
+$("#info_ejerc button").click(
+	function(){
+		agregarProyecto();
+		// $("input").val("");
+	}
+);
