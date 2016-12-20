81ec0900b01d08661b5f706a2642ae1768061e4e
Juan Alvarado <juaelalv@espol.edu.ec>
2016-12-20 14:01:23 -0500

    Visualizar Proyecto

---
+++ js/proyectos.js
@@ -0,0 +1,22 @@
+var proy;
+
+$(document).ready(function(){
+	u="json/proyecto.json"
+	$.getJSON(u, function(r){
+		proy=r.proyectos;
+		proy.forEach(function (p) {
+			footer=$("<div>",{"class":"panel-footer"});
+			p.tags.forEach(function (t) {
+				footer.append($("<a>", {"href":"#"}).text(t))
+			});
+			$("#divDondeVanLosPanels").append($("<div>", {"class":"panel panel-default"})
+				.append($("<div>",{"class":"panel-heading"})
+					.append($("<h3>").text("titulo: "+p.titulo)))
+				.append($("<div>",{"class":"panel-body"})
+					.append($("<p>").text(p.descripcion))
+					.append($("<p>").text("Fecha de Entrega "+p.fechaEntrega))
+					.append($("<a>",{"href":p.archivo}).text("Archivo adicional")))
+				.append(footer));
+		});
+	});
+});
