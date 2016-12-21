801f23f5fd540c90a496cfd24e57bf9e6c1eb620
Juan Alvarado <juaelalv@espol.edu.ec>
2016-12-21 10:18:05 -0500

    Revert "cambios erick"
    This reverts commit 783a8df71b6683ff6afd28b0b45d445793de47eb.

--- js/sdEstud.js
+++ js/sdEstud.js
@@ -37,73 +37,40 @@ function cargarA(){
 			agregarA(item);
 		}
 	});
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 }
 
 function agregarP(item){
 	$("#cuer").append($("<div>",{"class":"panel panel-info","id":"pan"}).append($("<div>",{"class":"panel-heading","id":"cab"}).text(item.titulo)));
-<<<<<<< HEAD
-	$("#cab").append($("<a>",{"href":"sb_estd2.html"}).append($("<button>",{"class":"btn btn-info"}).text(">")));
-=======
 	$("#cab").append($("<a>",{"href":"sb_estd2?"+item.id+".html"}).append($("<button>",{"class":"btn btn-info"}).text(">")));
->>>>>>> origin/master
 	$("#pan").append($("<div>",{"class":"panel-body","id":"panc"}).text("Autor: "+ item.autor));
 	$("#panc").append($("<p>").text(item.descri));
 	$("#panc").append($("<p>",{"style":"font-weight: bold;"}).text("Resuelto por: "+item.nEstd));
 	item.etiquetas.split(",").forEach(function(o){
 		$("#panc").append($("<span>",{"class":"bg-primary","style":"padding: 2px 5px 2px 5px;margin-right:1%;"}).text(o));
 	});
-<<<<<<< HEAD
-	var i = item.id;
-	localStorage.setItem("id",i);
-
-=======
 	
->>>>>>> origin/master
 }
 
 function agregarI(item){
 	$("#cuer").append($("<div>",{"class":"panel panel-warning","id":"pan"}).append($("<div>",{"class":"panel-heading","id":"cab"}).text(item.titulo)));
-<<<<<<< HEAD
-	$("#cab").append($("<a>",{"href":"sb_estd2.html"}).append($("<button>",{"class":"btn btn-warning"}).text(">")));
-=======
 	$("#cab").append($("<a>",{"href":"sb_estd2?"+item.id+".html"}).append($("<button>",{"class":"btn btn-warning"}).text(">")));
->>>>>>> origin/master
 	$("#pan").append($("<div>",{"class":"panel-body","id":"panc"}).text("Autor: "+ item.autor));
 	$("#panc").append($("<p>").text(item.descri));
 	$("#panc").append($("<p>",{"style":"font-weight: bold;"}).text("Resuelto por: "+item.nEstd));
 	item.etiquetas.split(",").forEach(function(o){
 		$("#panc").append($("<span>",{"class":"bg-primary","style":"padding: 2px 5px 2px 5px;margin-right:1%;"}).text(o));
 	});
-<<<<<<< HEAD
-	var i = item.id;
-	localStorage.setItem("id",i);
-	
-=======
->>>>>>> origin/master
 }
 
 function agregarA(item){
 	$("#cuer").append($("<div>",{"class":"panel panel-danger","id":"pan"}).append($("<div>",{"class":"panel-heading","id":"cab"}).text(item.titulo)));
-<<<<<<< HEAD
-	$("#cab").append($("<a>",{"href":"sb_estd2.html"}).append($("<button>",{"class":"btn btn-danger"}).text(">")));
-=======
 	$("#cab").append($("<a>",{"href":"sb_estd2?"+item.id+".html"}).append($("<button>",{"class":"btn btn-danger"}).text(">")));
->>>>>>> origin/master
 	$("#pan").append($("<div>",{"class":"panel-body","id":"panc"}).text("Autor: "+ item.autor));
 	$("#panc").append($("<p>").text(item.descri));
 	$("#panc").append($("<p>",{"style":"font-weight: bold;"}).text("Resuelto por: "+item.nEstd));
 	item.etiquetas.split(",").forEach(function(o){
 		$("#panc").append($("<span>",{"class":"bg-primary","style":"padding: 2px 5px 2px 5px;margin-right:1%;"}).text(o));
 	});
-<<<<<<< HEAD
-	var i = item.id;
-	localStorage.setItem("id",i);
-=======
->>>>>>> origin/master
 }
 
 $(document).ready(function(){
@@ -119,12 +86,6 @@ $(document).ready(function(){
 	$("#prin").click(cargarP);
 	$("#med").click(cargarM);
 	$("#avaz").click(cargarA);
-<<<<<<< HEAD
-})
-
-
-=======
 	
 })
 
->>>>>>> origin/master
