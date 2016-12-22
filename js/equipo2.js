b1139a4c964ecfcaceece9d00d4a9d75d45916e7
Erick Perez <erialper@espol.edu.ec>
2016-12-22 00:24:33 -0500

    profesores
    deded

--- js/equipo2.js
+++ js/equipo2.js
@@ -4,11 +4,11 @@ function cargar(){
 	$.getJSON(url,function(resul){
 		resul.forEach(function(item){
 			if(item.img==""){
-				$(".table tbody").append($("<tr></tr>").append("<td><img src=\"images/profile.png\" class=\"img-circle\" width=\"60px\" height=\"60px\"></td><td>"+ item.nombres + "</td><td>" + item.apellidos + "</td><td>" + item.correo + "</td><td>" + item. paralelo + "</td><td>" + item.oficina + "</td>"))
+				$(".table tbody").append($("<tr></tr>").append("<td><img src=\"images/profile.png\" class=\"img-circle\" width=\"50px\" height=\"50px\"></td><td>"+ item.nombres + "</td><td>" + item.apellidos + "</td><td>" + item.correo + "</td><td>" + item. paralelo + "</td><td>" + item.oficina + "</td>"));
 			}else{
-				$(".table tbody").append($("<tr></tr>").append("<td><img src="+ item.img +" class=\"img-circle\" width=\"60px\" height=\"60px\"></td><td>"+ item.nombres + "</td><td>" + item.apellidos + "</td><td>" + item.correo + "</td><td>" + item. paralelo + "</td><td>" + item.oficina + "</td>"))
+				$(".table tbody").append($("<tr></tr>").append("<td><img src="+ item.img +" class=\"img-circle\" width=\"50px\" height=\"50px\"></td><td>"+ item.nombres + "</td><td>" + item.apellidos + "</td><td>" + item.correo + "</td><td>" + item. paralelo + "</td><td>" + item.oficina + "</td>"));
 			}
-			$("td").css("text-align","center");
+			$("td").css({"text-align":"center","verticall-align":"center"});
 		})
 	})	
 }
