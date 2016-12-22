9ab31b3c0daf91e6a2b856a2dc0edbbfd5e1d34e
Erick Perez <erialper@espol.edu.ec>
2016-12-21 23:48:48 -0500

    profesores
    foto

--- js/equipo2.js
+++ js/equipo2.js
@@ -1,15 +1,16 @@
 function cargar(){
 	$(".table tbody").empty()
 	var url = "json/profesores.json"
-	var i = 0
 	$.getJSON(url,function(resul){
 		resul.forEach(function(item){
-			i++
-			$(".table tbody").append($("<tr></tr>").append("<td>"+ i +"</td><td>"+ item.nombres + "</td><td>" + item.apellidos + "</td><td>" + item.correo + "</td><td>" + item. paralelo + "</td><td>" + item.oficina + "</td>"))
+			if(item.img==""){
+				$(".table tbody").append($("<tr></tr>").append("<td><img src=\"images/profile.png\" class=\"img-circle\" width=\"60px\" height=\"60px\"></td><td>"+ item.nombres + "</td><td>" + item.apellidos + "</td><td>" + item.correo + "</td><td>" + item. paralelo + "</td><td>" + item.oficina + "</td>"))
+			}else{
+				$(".table tbody").append($("<tr></tr>").append("<td><img src="+ item.img +" class=\"img-circle\" width=\"60px\" height=\"60px\"></td><td>"+ item.nombres + "</td><td>" + item.apellidos + "</td><td>" + item.correo + "</td><td>" + item. paralelo + "</td><td>" + item.oficina + "</td>"))
+			}
+			$("td").css("text-align","center");
 		})
-		
-	})
-	
+	})	
 }
 
 $(document).ready(cargar)
